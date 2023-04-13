2ac4864f88dedd54bf438b4f9af239e47a8116d7
Fyodor Kurlyuk <qrluke@proton.me>
2023-04-13 07:28:39 +0700

    reformat code

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,19 +1,19 @@
-(function(document) {
+(function (document) {
     var metas = document.getElementsByTagName('meta'),
-        changeViewportContent = function(content) {
+        changeViewportContent = function (content) {
             for (var i = 0; i < metas.length; i++) {
                 if (metas[i].name == "viewport") {
                     metas[i].content = content;
                 }
             }
         },
-        initialize = function() {
+        initialize = function () {
             changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
         },
-        gestureStart = function() {
+        gestureStart = function () {
             changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
         },
-        gestureEnd = function() {
+        gestureEnd = function () {
             initialize();
         };
 
